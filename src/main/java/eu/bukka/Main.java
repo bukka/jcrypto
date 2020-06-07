06ca3b30c4902fa4e2a65c27d549eb6a8a28e2d5
Jakub Zelenka <bukka@php.net>
2020-06-07 16:05:26 +0100

    Add Security setup

--- src/main/java/eu/bukka/Main.java
+++ src/main/java/eu/bukka/Main.java
@@ -2,10 +2,16 @@ package eu.bukka;
 
 import eu.bukka.cli.CMSCommand;
 import eu.bukka.cli.TopCommand;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import picocli.CommandLine;
+
+import java.security.Security;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Security.setProperty("crypto.policy", "unlimited");
+        Security.addProvider(new BouncyCastleProvider());
+
         CommandLine cmd = new CommandLine(new TopCommand())
                 .addSubcommand("cms", new CMSCommand());
 
