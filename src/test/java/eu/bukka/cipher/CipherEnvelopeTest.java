2809fca9d195b68d2f5c9dfbb9c866e8859ac594
Jakub Zelenka <bukka@php.net>
2020-11-08 18:40:14 +0000

    Add initial empty test for CipherEnvelope

---
+++ src/test/java/eu/bukka/cipher/CipherEnvelopeTest.java
@@ -0,0 +1,16 @@
+package eu.bukka.cipher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CipherEnvelopeTest {
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void decrypt() {
+    }
+}
