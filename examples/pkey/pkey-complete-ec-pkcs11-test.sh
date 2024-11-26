6e126402b35898804a4d39e3ba862c3fb1f95730
Jakub Zelenka <bukka@php.net>
2024-11-26 22:06:53 +0100

    Add and fix PKCS11 sign and verify

--- examples/pkey/pkey-complete-ec-pkcs11-test.sh
+++ examples/pkey/pkey-complete-ec-pkcs11-test.sh
@@ -7,13 +7,25 @@ jcrypto_curve_name=$1
 if [ -z "$jcrypto_curve_name" ]; then
   jcrypto_curve_name="secp256r1"
 fi
-jcrypto_out_pub_key_file="$jcrypto_tmp_dir/out-pkey-ec-${jcrypto_curve_name}-pub-key-pkcs11.der"
+jcrypto_pub_key_file="$jcrypto_tmp_dir/out-pkey-ec-${jcrypto_curve_name}-pub-key-pkcs11.der"
+jcrypto_out_sig_file="$jcrypto_tmp_dir/out-pkey-ecdsa-basic-sig.txt"
 
 jcrypto_pkcs11_setup pkey-complete
 
 echo "JCRYPTO GENERATE KEY"
-jcrypto pkey generate --public-key-file "$jcrypto_out_pub_key_file" --private-key-alias JavaTestECKey \
+jcrypto pkey generate --public-key-file "$jcrypto_pub_key_file" --private-key-alias JavaTestECKey \
   --key-store-password 1234 --key-store-name PKCS11 --algorithm EC --parameters $jcrypto_curve_name \
   --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
 
-jcrypto_cat "$jcrypto_out_pub_key_file" | base64
+jcrypto_cat "$jcrypto_pub_key_file" | base64
+
+echo "JCRYPTO SIGN"
+jcrypto pkey sign -i "$jcrypto_this_dir/in-pkey-data.txt" -a SHA256withECDSA -o "$jcrypto_out_sig_file" \
+  --private-key-alias JavaTestECKey --key-store-password 1234 --key-store-name PKCS11 \
+  --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
+
+echo "JCRYPTO VERIFY"
+jcrypto pkey verify -i "$jcrypto_this_dir/in-pkey-data.txt" -a SHA256withECDSA \
+  --signature-file "$jcrypto_out_sig_file" --public-key-file "$jcrypto_pub_key_file" \
+  --key-store-password 1234 --key-store-name PKCS11 \
+  --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
