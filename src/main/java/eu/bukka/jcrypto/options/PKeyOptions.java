326d9aa159a9d245a11c8ebb8c94d813fd788087
Jakub Zelenka <bukka@php.net>
2024-11-26 11:50:51 +0100

    Add pkey signature

--- src/main/java/eu/bukka/jcrypto/options/PKeyOptions.java
+++ src/main/java/eu/bukka/jcrypto/options/PKeyOptions.java
@@ -1,17 +1,26 @@
 package eu.bukka.jcrypto.options;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface PKeyOptions extends CommonOptions {
     String getAlgorithm();
 
     String getPrivateKeyAlias();
 
-    String getPublicKeyAlias();
-
     File getPrivateKeyFile();
 
+    byte[] getPrivateKeyFileData() throws IOException;
+
+    String getPublicKeyAlias();
+
     File getPublicKeyFile();
+
+    byte[] getPublicKeyFileData() throws IOException;
+
+    File getSignatureFile();
+
+    byte[] getSignatureFileData() throws IOException;
 
     String getKeyStoreName();
 
