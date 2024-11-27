0c6d51d25f9143da02872eb2f94c0e234123313e
Jakub Zelenka <bukka@php.net>
2024-11-27 13:55:32 +0100

    Add test for sign and verify always loading key store

--- examples/pkey/pkey-sign-verify-ec-pkcs11-test.sh
+++ examples/pkey/pkey-sign-verify-ec-pkcs11-test.sh
@@ -7,25 +7,22 @@ jcrypto_curve_name=$1
 if [ -z "$jcrypto_curve_name" ]; then
   jcrypto_curve_name="secp256r1"
 fi
-jcrypto_pub_key_file="$jcrypto_tmp_dir/out-pkey-ec-${jcrypto_curve_name}-pub-key-pkcs11.der"
 jcrypto_out_sig_file="$jcrypto_tmp_dir/out-pkey-ecdsa-basic-sig.txt"
 
-jcrypto_pkcs11_setup pkey-complete
+jcrypto_pkcs11_setup pkey-sign-verify
 
 echo "JCRYPTO GENERATE KEY"
-jcrypto pkey generate --public-key-file "$jcrypto_pub_key_file" --private-key-alias JavaTestCompleteECKey \
+jcrypto pkey generate --private-key-alias JavaTestSignVerifyECKey \
   --key-store-password 1234 --key-store-name PKCS11 --algorithm EC --parameters $jcrypto_curve_name \
   --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
 
-jcrypto_cat "$jcrypto_pub_key_file" | base64
-
 echo "JCRYPTO SIGN"
 jcrypto pkey sign -i "$jcrypto_this_dir/in-pkey-data.txt" -a SHA256withECDSA -o "$jcrypto_out_sig_file" \
-  --private-key-alias JavaTestCompleteECKey --key-store-password 1234 --key-store-name PKCS11 \
+  --private-key-alias JavaTestSignVerifyECKey --key-store-password 1234 --key-store-name PKCS11 \
   --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
 
 echo "JCRYPTO VERIFY"
 jcrypto pkey verify -i "$jcrypto_this_dir/in-pkey-data.txt" -a SHA256withECDSA \
-  --signature-file "$jcrypto_out_sig_file" --public-key-file "$jcrypto_pub_key_file" \
+  --signature-file "$jcrypto_out_sig_file" --public-key-alias JavaTestSignVerifyECKey \
   --key-store-password 1234 --key-store-name PKCS11 \
   --provider-name SunPKCS11 --provider-config-file "$jcrypto_pkcs11_java_config"
